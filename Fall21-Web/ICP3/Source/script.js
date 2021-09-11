8ed0828481135707309937307d0747e368d06e09
gjw6k <gjw6k@umsystem.edu>
2021-09-11 13:49:50 -0500

    changes

--- Fall21-Web/ICP3/Source/script.js
+++ Fall21-Web/ICP3/Source/script.js
@@ -1,14 +1,45 @@
-const userChoice = document.getElementById("scissor").onclick; //user choice
-prompt(userChoice)
+var userChoice;
+function reply_click(obj) {
+    userChoice = obj.id;
+    const computerChoice = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
 
-let computerChoice = Math.random();                         //computer choice
-if (computerChoice < 0.34) {
-    computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-    computerChoice = "paper";
-} else {
-    computerChoice = "scissors";
-}
-if( userChoice == computerChoice ){
-    alert("Both are matched")
+    let compare = function (choice1, choice2) {                // written a function for comparing values
+        if (choice1 === choice2) {
+            alert("Both matched");
+        }
+        if (choice1 === "rock") {
+            if (choice2 === "scissors") {
+                alert("You selected rock and computer selected scissors");
+                alert("You Won");
+            } else {
+                alert("You selected rock and computer selected paper")
+                alert("Computer Won");
+            }
+        }
+        if (choice1 === "paper") {
+            if (choice2 === "rock") {
+                alert("You selected paper and computer selected rock");
+                // paper wins
+                alert( "You Won");
+            } else {
+                // scissors wins
+                alert("You selected paper and computer selected scissors")
+                alert("Computer Won");
+            }
+        }
+        if (choice1 === "scissors") {
+            if (choice2 === "rock") {
+                alert("You selected scissors and computer selected rock");
+                // rock wins
+                alert("Computer Won");
+            } else {
+                // scissors wins
+                alert("You selected scissors and computer selected paper")
+                alert("You Won");
+            }
+        }
+    }
+//document.write("<p>User Choice:" + " " + userChoice + "</p>");
+//document.write("<p>Computer Choice:" + " " + computerChoice + "</p>");        // displaying values
+    compare(userChoice, computerChoice);
 }
